8f55116330d046e7fd5034b7afac85abf3951450
mucacran <el.mucacran.rasta@hotmail.com>
2025-03-29 20:01:49 -0500

    nuevos cambios

--- Program.cs
+++ Program.cs
@@ -11,9 +11,7 @@
 
 using w11finalProject;
 
-Console.WriteLine("\n======================\nWeek 11 Prove: Final Project - Draft Submission 1\n======================");
-//PerformanceTests.Run();
-//FunctionalTests.Run();
+Console.WriteLine("\n=======================================================\n|| Week 11 Prove: Final Project - Draft Submission 1 ||\n=======================================================");
 AQueueTests.Run();
 QueuePerformance.Run();
 Console.WriteLine("======================\n");
